6608445db02b0189abd0eb755c1ac43e416a0881
somashekhar <somashekharm4159@gmail.com>
2025-03-10 22:44:31 +0530

    handler func return removed

--- internal/handlers/blog_handler.go
+++ internal/handlers/blog_handler.go
@@ -7,69 +7,57 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func (h *handler) CreateBlog(c *fiber.Ctx) {
+func (h *handler) CreateBlog(c *fiber.Ctx) error {
 	blog := new(models.Blog)
 	if err := c.BodyParser(blog); err != nil {
-		c.Status(400).JSON(fiber.Map{"error": err.Error()})
-		return
+		return c.Status(400).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	err := h.service.CreateBlogService(blog)
 	if err != nil {
-		c.Status(500).JSON(fiber.Map{"error": "Failed to create blog"})
-		return
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to create blog"})
 	}
 
-	c.Status(201).JSON(blog)
-	return
+	return c.Status(201).JSON(blog)
 }
 
-func (h *handler) GetAllBlogs(c *fiber.Ctx) {
+func (h *handler) GetAllBlogs(c *fiber.Ctx) error {
 	blogs, err := h.service.GetAllBlogsService()
 	if err != nil {
-		c.Status(500).JSON(fiber.Map{"error": "Failed to fetch blogs"})
-		return
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to fetch blogs"})
 	}
-	c.JSON(blogs)
-	return
+	return c.JSON(blogs)
 }
 
-func (h *handler) GetBlogByID(c *fiber.Ctx) {
+func (h *handler) GetBlogByID(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 	blog, err := h.service.GetBlogByIDService(uint(id))
 	if err != nil {
-		c.Status(404).JSON(fiber.Map{"error": "Blog not found"})
-		return
+		return c.Status(404).JSON(fiber.Map{"error": "Blog not found"})
 	}
-	c.JSON(blog)
-	return
+	return c.JSON(blog)
 }
 
-func (h *handler) UpdateBlog(c *fiber.Ctx) {
+func (h *handler) UpdateBlog(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 	blog, err := h.service.GetBlogByIDService(uint(id))
 	if err != nil {
-		c.Status(404).JSON(fiber.Map{"error": "Blog not found"})
-		return
+		return c.Status(404).JSON(fiber.Map{"error": "Blog not found"})
 	}
 
 	if err := c.BodyParser(&blog); err != nil {
-		c.Status(400).JSON(fiber.Map{"error": err.Error()})
-		return
+		return c.Status(400).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	h.service.UpdateBlogService(&blog)
-	c.JSON(blog)
-	return
+	return c.JSON(blog)
 }
 
-func (h *handler) DeleteBlog(c *fiber.Ctx) {
+func (h *handler) DeleteBlog(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 	err := h.service.DeleteBlogService(uint(id))
 	if err != nil {
-		c.Status(500).JSON(fiber.Map{"error": "Failed to delete blog"})
-		return
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to delete blog"})
 	}
-	c.SendStatus(204)
-	return
+	return c.SendStatus(204)
 }
